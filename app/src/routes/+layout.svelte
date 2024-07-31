a5522247c4ac73e1559ab15e3dfc2fff757cb460
Akuzike8 <akuzikenchembe8@gmail.com>
2024-07-31 18:07:36 +0200

    update

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,4 +2,10 @@
     import "../app.css";
 </script>
 
+<header class="border-b max-w-6xl mx-auto">
+    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <a class="text-xl text-blue-500 font-bold" href="#">PostalNav</a>
+    </nav>
+</header>
+
 <slot/>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>PostalNav</h1>
-<p>Postal codes at your fingertips</p>
+
+<main class="max-w-6xl mx-auto">
+    <section class="my-5 text-center">
+          <h1 class="text-2xl tracking-tight text-gray-900 ">Postal codes at your fingertips</h1>
+    </section>
+    
+    <form class="max-w-md mx-auto my-10">   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-100 dark:placeholder-gray-400 dark:text-gray-500 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Area..." required />
+            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+    </form>
+    
+    <div class="relative overflow-x-auto flex">
+        <table class="w-50 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-500 rounded-md">
+            <thead class="text-xs text-grayjm-700 uppercase bg-gray-50 dark:bg-gray-100 dark:text-gray-900">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Area
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Postal code
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        District
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Region
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Settlement
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="odd:bg-gray border-b odd:dark:bg-gray-300 odd:dark:border-gray-300">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray-500">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        Silver
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        
+    </div>
+</main>
+
